42bd286566fc0b1bd4e2a8ea93d20545edadd1b5
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-11-29 01:37:36 -0500

    made button dissapear after installing

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -28,18 +28,13 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  ({ request }) => request.destiniation === 'images',
-  new CacheFirst({
-
-    cacheName: 'image-cache',
+  ({ request }) => ['style', 'script', 'worker', 'image'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
     plugins: [
       new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60
-      }),
-    ],
-  })
+        statuses: [0, 200]
+      })
+    ]
+  }) 
 );
